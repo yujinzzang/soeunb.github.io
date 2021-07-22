1e88ecbbc9a0de6077b434bc128d192f21bb9e33
soeun <soeunb@>
2021-07-23 00:29:57 +0900

    clean push

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("test");
+window.onload = function(){
+    const cardWrap = document.querySelector('.calendar');
+    const modeToggle = document.querySelector('.switch-label');
+    if( cardWrap &&  modeToggle ){
+        modeToggle.addEventListener("click", toggleClick);
+    }
+    function toggleClick(){
+        if( cardWrap.className === 'calendar light' ){
+            cardWrap.classList.replace('light', 'dark');
+        }else{
+            cardWrap.classList.replace('dark', 'light');
+        }
+        console.log('aa');
+    }
+}
