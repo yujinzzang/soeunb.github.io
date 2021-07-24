b1a9f1c0e90a4880d710b3b77fe6cae9ebdd99f2
soeun <soeunb@>
2021-07-25 01:55:28 +0900

    add card 추가

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
 window.onload = function(){
+
+    const mainCard = document.querySelector('.calendar.main');
+    const addCard = document.querySelector('.calendar.add');
+
     const cardWrap = document.querySelector('.calendar');
     const modeToggle = document.querySelector('.switch-label');
-    if( cardWrap &&  modeToggle ){
-        modeToggle.addEventListener("click", toggleClick);
+    if( cardWrap && modeToggle ){
+        modeToggle.addEventListener('click', toggleClick);
     }
+
+    const addBtn = document.querySelector('.cl_add');
+    addBtn.addEventListener('click', function(){
+        displayCard('add');
+    }, false);
+    
+    const closeBtn = document.querySelector('.close_btn');
+    closeBtn.addEventListener('click', function(){
+        displayCard('main');
+    }, false);
+
+    const applyBtn = document.querySelector('.apply_btn');
+    applyBtn.addEventListener('click', function(){
+        displayCard('main');
+    }, false);
+
     function toggleClick(){
         const modeToggleChk = document.querySelector('.switch-input').checked;
         if( !modeToggleChk ){
             modeToggle.setAttribute('data-on-off', 'on');
-            cardWrap.classList.remove('light');
-            cardWrap.classList.add('dark');
+            cardWrap.classList.replace('light', 'dark');
         }else{
             modeToggle.setAttribute('data-on-off', 'off');
-            cardWrap.classList.remove('dark');
-            cardWrap.classList.add('light');
+            cardWrap.classList.replace('dark', 'light');
+        }
+    }
+
+    function displayCard( type ){   
+        if( type === 'main' ){
+            mainCard.style.display = 'block';
+            addCard.style.display = 'none';
+        }else if( type === 'add' ){
+            mainCard.style.display = 'none';
+            addCard.style.display = 'block';
         }
     }
 }
